fad0a89f391aa7ce92fe040c182bb502287ea4a1
Julio Gutierrez <413330+bubuntux@users.noreply.github.com>
2024-04-30 09:25:56 -0500

    echo test

--- modules/ipu6/ipu6.sh
+++ modules/ipu6/ipu6.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 echo "XXX"
 
+echo "$0"
+echo "$1"
+echo "$2"
+
 # read a single variable from the configuration
 VAR=$(echo "$1" | yq -I=0 ".var") # `-I=0` makes sure the output isn't indented
 echo "$VAR"
